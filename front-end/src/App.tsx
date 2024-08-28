3755c2e112bd6c88f0675611704c0aa4f290f321
Witnney <witnneygabriel13@gmail.com>
2024-08-28 15:27:49 -0300

    chore: add initial setup

---
+++ front-end/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="">
+      <Button>OI</Button>
+    </div>
+  );
+}
+
+export default App;
