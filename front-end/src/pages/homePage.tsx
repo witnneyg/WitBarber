4c34c0342c4ac41d33d0d86ecffb933805d84be4
Witnney <witnneygabriel13@gmail.com>
2024-08-31 16:27:22 -0300

    feat: add bookings title

--- front-end/src/pages/homePage.tsx
+++ front-end/src/pages/homePage.tsx
@@ -26,6 +26,10 @@ export function HomePage() {
         />
       </div>
 
+      <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">
+        Agendamentos
+      </h2>
+
       <Card>
         <CardContent className="flex justify-between p-0">
           <div className="flex flex-col gap-2 py-5 pl-5">
