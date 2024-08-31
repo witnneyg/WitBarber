6592775938cd2da6942dfe4f087c354aa64dc9e2
Witnney <witnneygabriel13@gmail.com>
2024-08-31 16:23:43 -0300

    feat: add bookings item

--- front-end/src/pages/homePage.tsx
+++ front-end/src/pages/homePage.tsx
@@ -1,4 +1,7 @@
+import { AvatarImage, Avatar } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { SearchIcon } from "lucide-react";
 
@@ -21,8 +24,27 @@ export function HomePage() {
           alt="Agende nos melhorescom FSW Barber"
           className="object-cover rounded-xl"
         />
-        {/* fill */}
       </div>
+
+      <Card>
+        <CardContent className="flex justify-between p-0">
+          <div className="flex flex-col gap-2 py-5 pl-5">
+            <Badge className="w-fit"></Badge>
+            <h3 className="font-semibold">Corte de Cabelo</h3>
+            <div className="flex items-center">
+              <Avatar className="h-6 w-6">
+                <AvatarImage src="" />
+              </Avatar>
+              <p className="text-sm">Barbearia FSW</p>
+            </div>
+          </div>
+          <div className="flex fex-col items-center justify-center px-5 border-l-2 border-solid">
+            <p className="text-sm">Agosto</p>
+            <p className="text-2xl">05</p>
+            <p className="text-sm">20:00</p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
