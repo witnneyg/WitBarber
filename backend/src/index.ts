aa614f7b344cd5a6024dc64be9e879817d3b8053
Witnney <witnneygabriel13@gmail.com>
2024-09-02 18:15:35 -0300

    feat: add initial cors setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import express from "express";
 
+import cors from "cors";
+
 import barberRoutes from "./controllers/barber-controller";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 const port = 8888;
 
-app.use("/", barberRoutes);
+app.use("/barbershop", barberRoutes);
 
 app.listen(port, () => {
   console.log(`rodando na porta ${port}`);
