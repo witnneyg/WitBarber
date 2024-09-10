075bdb8ec26e0a9657ac1b845b0e8c102bf72432
Witnney <witnneygabriel13@gmail.com>
2024-09-10 17:37:45 -0300

    fix: change the footer title

--- front-end/src/components/footer.tsx
+++ front-end/src/components/footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       <Card>
         <CardContent className="px-5 py-6">
           <p className="text-sm text-gray-400">
-            2024 Copyright <span className="font-bold">FSW</span> Barber
+            © 2024 Copyright <span className="font-bold">FSW</span> Barber
           </p>
         </CardContent>
       </Card>
