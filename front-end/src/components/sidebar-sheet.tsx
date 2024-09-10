14b076b5c48e9375f4eb3d75609784cbca0c4f80
Witnney <witnneygabriel13@gmail.com>
2024-09-10 17:36:46 -0300

    feat: add dialog trigger to menu

--- front-end/src/components/sidebar-sheet.tsx
+++ front-end/src/components/sidebar-sheet.tsx
@@ -1,9 +1,17 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "@/constants/search";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Link } from "react-router-dom";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 export function SidebarSheet() {
   return (
@@ -12,7 +20,28 @@ export function SidebarSheet() {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center py-5 border-b border-solid gap-3">
+      <div className="flex items-center py-5 border-b border-solid gap-3 justify-between">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant="outline" className="gap-1 font-bold">
+              <img src="" alt="Google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
         <Avatar>
           <AvatarImage src="" />
         </Avatar>
